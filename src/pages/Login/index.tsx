cb17dd21949883f7264a8196ba58fb364b3322a5
Leonardo Luz <39962306+leouluz@users.noreply.github.com>
2022-02-10 00:17:22 -0300

    added styled-components and created login screen

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styled'
+
+export default function Login() {
+  return (
+    <Container>
+      <p>Tela de Login</p>
+    </Container>
+  );
+}
